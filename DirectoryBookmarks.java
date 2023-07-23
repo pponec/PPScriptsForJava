a60e7d5483632d7e234abd6c03ae158da284d0aa
Pavel Ponec <pponec@gmail.com>
2023-07-23 22:31:40 +0200

    Upgrade

--- DirectoryBookmarks.java
+++ DirectoryBookmarks.java
@@ -8,6 +8,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Optional;
 import java.util.regex.Pattern;
 
@@ -15,7 +16,7 @@ public class DirectoryBookmarks {
 
     private final String homePage = "https://github.com/pponec/DirectoryBookmarks";
     private final String appName = getClass().getName();
-    private final String appVersion = "1.4";
+    private final String appVersion = "1.5";
     private final String storeName = ".directory-bookmarks.csv";
     private final char separator = '\t';
     private final char comment = '#';
@@ -38,7 +39,7 @@ public class DirectoryBookmarks {
                 if (args.length < 3)
                     o.printHelpAndExit();
                 String[] msg = Arrays.copyOfRange(args, 3, args.length);
-                o.save(args[1], args[2], msg);
+                o.save(args[2], args[1], msg);
                 break;
             case "d":
                 if (args.length < 2)
@@ -58,6 +59,9 @@ public class DirectoryBookmarks {
             case "i4j":
                 o.printInstall(true);
                 break;
+            case "e":
+                o.removeAllDeprecatedDiredtories();
+                break;
             case "c":
                 o.compile();
                 break;
@@ -69,8 +73,8 @@ public class DirectoryBookmarks {
     private void printHelpAndExit() {
         String bashrc = "~/.bashrc";
         System.out.println("Script '" + appName + "' v" + appVersion + " (" + homePage + ")");
-        System.out.println("Usage: " + appName + " [rwlc] bookmark directory optionalComment");
-        System.out.println("Integrate the script to Ubuntu: " + appName + " i >> " + bashrc + " && . " + bashrc);
+        System.out.println("Usage: java " + appName + ".java [rwldec] bookmark directory optionalComment");
+        System.out.println("Integrate the script to Ubuntu: java " + appName + ".java i >> " + bashrc + " && . " + bashrc);
         System.exit(1);
     }
 
@@ -118,7 +122,7 @@ public class DirectoryBookmarks {
         save(key, "");
     }
 
-    private void save(String dir, String key, String... comments) throws IOException {
+    private void save(String key, String dir, String... comments) throws IOException {
         if (key.contains(String.valueOf(separator))) {
             throw new IllegalArgumentException("the key contains a tab");
         }
@@ -169,6 +173,34 @@ public class DirectoryBookmarks {
 
     private File getStoreFileTemplate() throws IOException {
         return File.createTempFile("storeName", "", new File(homeDir));
+    }
+
+    private void removeAllDeprecatedDiredtories() throws IOException {
+        var keys = Collections.<String>emptyList();
+        try (BufferedReader reader = new BufferedReader(new FileReader(getStoreFile()))) {
+            keys = reader.lines()
+                    .filter(line -> !line.startsWith(String.valueOf(comment)))
+                    .map(line -> line.substring(0, line.indexOf(separator)))
+                    .toList();
+        }
+        keys.stream()
+                .filter(key -> {
+                    try {
+                        var dir = getDirectory(key, "");
+                        return dir.isEmpty() || !new File(dir).isDirectory();
+                    } catch (IOException e) {
+                        throw new IllegalStateException(e);
+                    }
+                })
+                .forEach(key -> {
+                    try {
+                        var msg = "Removed: %s\t%s".formatted(key, getDirectory(key, "?"));
+                        System.out.println(msg);
+                        delete(key);
+                    } catch (IOException e) {
+                        throw new IllegalStateException(e);
+                    }
+                });
     }
 
     /** Compile the script and build it to the executable JAR file */
