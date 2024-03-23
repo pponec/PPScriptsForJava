7d224a2a64e2ec47ec53560f271ce84deb6769bd
pponec <pponec@gmail.com>
2024-03-23 22:44:51 +0100

    Documentation

--- src/main/java/net/ponec/script/SqlExecutorKt.sh
+++ src/main/java/net/ponec/script/SqlExecutorKt.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-# Sample SQL scripting for the Bash and Java v17.
-# How to install Kotlin for Linux: sudo snap install --classic kotlin
+# Sample SQL scripting for the Bash and Kotlin.
+# How to install Kotlin to Ubuntu: sudo snap install --classic kotlin
 
 set -e
+cd $(dirname $0)
+version=2.2.224
+driver=$HOME/.m2/repository/com/h2database/h2/$version/h2-$version.jar
+mvn=../../../../../../mvnw.sh
+if [ ! -e "$driver" ]; then
+   sh $mvn dependency:get -Dartifact=com.h2database:h2:$version
+fi
+
 ktFile="SqlExecutorKt.kt"
 ktsFile="${ktFile}s"
-
 cp "$ktFile" "$ktsFile"
 sed -i '0,/^\/\/KTS\/\//s// /' "$ktsFile"
-kotlin -cp ../lib/h2-2.2.224.jar "$ktsFile" "$@"
+kotlin -cp $driver "$ktsFile" "$@"
 rm "$ktsFile"
