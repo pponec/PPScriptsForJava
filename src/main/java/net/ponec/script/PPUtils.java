29de7be7ac671e4164b68c56982689253a9b630c
pponec <pponec@gmail.com>
2024-04-26 16:19:06 +0200

    Code cleaning

--- src/main/java/net/ponec/script/PPUtils.java
+++ src/main/java/net/ponec/script/PPUtils.java
@@ -58,7 +58,7 @@ public final class PPUtils {
 
     private final String appName = getClass().getSimpleName();
 
-    private final String appVersion = "1.1.0";
+    private final String appVersion = "1.1.1";
 
     private final Class<?> mainClass = getClass();
 
